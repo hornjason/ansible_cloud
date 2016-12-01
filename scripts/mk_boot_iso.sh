ce7d954076ba45b917fb3087d3b412930729034c
ansible_team <ansible_team>
2016-12-01 13:16:47 -0500

    updated mk_boot_iso.sh

--- scripts/mk_boot_iso.sh
+++ scripts/mk_boot_iso.sh
@@ -1,26 +1,30 @@
-mount_dir="/media"
+
 
 if [[ -z $2 ]]; then
   echo "Must Specify iso image"
   exit
 fi
 
+mkdir -p /test
+mount_dir=/test
 iso="$2"
 iso_name=`echo $iso|awk -F'.iso' '{print $1}'`
 # create dump_dir
-dump_dir="$iso_name"
+dump_dir="$PWD/$iso_name"
 
 case "$1" in
   dump)
-        sudo mount $iso $mount_dir
+        mount $iso $mount_dir
         mkdir $dump_dir
         # extract files
-        cd $mount_dir;tar cf - . |  (cd /home/raleigh/jhorn2/iso/$dump_dir; tar xfp -);cd -
+        cd $mount_dir;tar cf - . |  (cd $dump_dir; tar xfp -);cd -
+        umount $mount_dir
         ;;
  cr*)
         # change ks.cfg and update isolinux/isolinux.cfg boot menu if needed
         echo "IM HERE"
-        iso_name="$2"
-        cd ~/iso/$iso_name; mkisofs -o ../${iso_name}.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -v -T .
+        #iso_name="$2"
+        cd ./$iso_name
+	mkisofs -o ../${iso_name}.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J -v -T .
         ;;
 esac
